e9a34702cec121cbdc39f5abc3c6afdc253b6f3c
Fahad Khan <139643409+fkb003@users.noreply.github.com>
2024-08-16 11:03:22 +0600

    Refactor command processing into it's own function

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ tasks = [] # List to store tasks
 done = [] # List to store completed tasks
 skipped = [] # List to store skipped tasks
 
-while True:
-    # Print tasks
-    for i, task in enumerate(tasks):
-        print(f'{i+1:3}. {task}')
-    
-    # Input new tasks, if empty, break loop.
-    cmd = input('>>> ')
-    if not cmd: break
+def act(cmd):
+    if not cmd: exit()
     elif cmd[0] == '.':
         # Pop task from tasks to done list
         done.append(tasks.pop(int(cmd[1:])-1))
     elif cmd[0] == '#':
         skipped.append(tasks.pop(int(cmd[1:])-1))
     else: tasks.append(cmd)
+
+while True:
+    # Display tasks
+    for i, task in enumerate(tasks):
+        print(f'{i+1:3}. {task}')
+
+    # Take commands and process them
+    command = input('>>> ')
+    act(command)
