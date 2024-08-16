86e17ae01c322ab03ff8297344149f52fa87c871
Fahad Khan <139643409+fkb003@users.noreply.github.com>
2024-08-16 10:35:22 +0600

    Add functionality to mark tasks as skipped

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 tasks = [] # List to store tasks
 done = [] # List to store completed tasks
+skipped = [] # List to store skipped tasks
 
 while True:
     # Print tasks
@@ -12,4 +13,6 @@ while True:
     elif cmd[0] == '.':
         # Pop task from tasks to done list
         done.append(tasks.pop(int(cmd[1:])-1))
+    elif cmd[0] == '#':
+        skipped.append(tasks.pop(int(cmd[1:])-1))
     else: tasks.append(cmd)
