e29abf9f13f5493a355d98c26f506c4842d32ac6
Fahad Khan <139643409+fkb003@users.noreply.github.com>
2024-08-27 13:57:21 +0600

    Add error handling & improve index interpretation

--- action.py
+++ action.py
@@ -1,5 +1,6 @@
 def move(index: int, origin: list, destination: list) -> None:
-    destination.append(origin.pop(index))
+    try: destination.append(origin.pop(index))
+    except: input('That action is not possible!\n')
 
 def complete(index: int, selected_lists: list, lists: dict) -> None:
     move(index, lists[selected_lists[0]], lists['done'])
@@ -11,20 +12,31 @@ def add_task(task: str, selected_lists: list, lists: dict) -> None:
     lists[selected_lists[0]].append(task)    
 
 def add_list(list_name: str, selected_lists: list, lists: dict) -> None:
-    lists.update({list_name: []})
-    selected_lists.insert(-2, list_name)
-    change_list(list_name, selected_lists, lists)
+    if type(list_name) != str:
+        input("Indices can't be used as list names!\n")
+    elif list_name in lists:
+        input("List already exists!\n")
+    else:
+        lists.update({list_name: []})
+        selected_lists.insert(-2, list_name)
+        change_list(list_name, selected_lists, lists)
 
 def change_list(l, selected_lists: list, lists: dict) -> None:
+    visible_lists = selected_lists[1:]
     if type(l) == str:
-        selected_lists[0] = l
+        if l in lists: selected_lists[0] = l
+        else: input("List doesn't exist!\n")
     else:
-        selected_lists[0] = selected_lists[l+1]
+        if -len(visible_lists) <= l < len(visible_lists):
+            selected_lists[0] = visible_lists[l]
+        else: input("List doesn't exist!\n")
+
     
 def stop(additional: str, selected_lists: list, lists: dict):
     exit()
+
 menu = {
-       '.': complete,
+       '.': complete, 
         '#': skip,
         '!': stop,
         '+': add_list,
--- interpret.py
+++ interpret.py
@@ -2,23 +2,27 @@ import action
 
 def interpret(cmd: str, selected_lists: list, lists: dict) -> None: 
     """Call other functions and pass identified arguments"""
-    if cmd[0] in action.menu:
+    if not cmd: pass
+    elif cmd[0] in action.menu:
         operator = cmd[0]
-        if is_index(cmd[1:]):
-            operand = int(cmd[1:])
-            if operand > 0: operand -= 1
+        index_test = is_index(cmd[1:])
+        if index_test[0]:
+            operand = index_test[1]
         else:
             operand = cmd[1:]
         action.menu[operator](operand, selected_lists, lists)
     else:
         action.menu['add'](cmd, selected_lists, lists)
 
-def is_index(s: str) -> bool: 
+def is_index(s: str) -> tuple: 
     """Test if given string is a valid index"""
     if not s:
-        return False
+        return (False,)
     if s.isdigit():
-        return True
+        if int(s)>0:
+            return (True, int(s)-1)
+        else:
+            return (True, int(s))
     if s[0]=='-' and s[1:].isdigit():
-        return True
-    return False
+        return (True, int(s))
+    return (False,)
