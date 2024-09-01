84d8ae19f843409a0edb947a5993d2aa83e410cf
Fahad Khan <139643409+fkb003@users.noreply.github.com>
2024-09-01 14:59:13 +0600

    Improve column spacing & auto-hide certain columns
    - Max column width is 80
    - Removed max table width
    - 'done' and 'skipped' will be hidden if empty

--- display.py
+++ display.py
@@ -2,10 +2,16 @@ import os
 
 def display(selected_lists: list, lists: dict) -> None:
     clear_screen()
-    terminal_size = min(120, os.get_terminal_size().columns)
+    terminal_size = os.get_terminal_size().columns
     visible_lists = selected_lists[1:]
+    if not lists['done']:
+        visible_lists.pop(-2)
+        if selected_lists[0] == 'done': selected_lists[0] = 'default'
+    if not lists['skipped']:
+        visible_lists.pop(-1)
+        if selected_lists[0] == 'skipped': selected_lists[0] = 'default'
     max_length = 0
-    max_width = int((terminal_size/len(visible_lists))-3.5)
+    max_width = min(80, int((terminal_size/len(visible_lists))-3.5))
     for l in visible_lists:
         length = len(lists[l])
         if length > max_length:
@@ -13,7 +19,7 @@ def display(selected_lists: list, lists: dict) -> None:
     
     dash_length = (len(visible_lists) * (max_width+3))+1
     print('-' * dash_length)
-    print_headers(selected_lists, max_width)
+    print_headers(selected_lists[0], visible_lists,  max_width)
     print('-' * dash_length)
     if max_length == 0:
         print(f"|{'No tasks available. Add some.':^{dash_length-2}}|")
@@ -30,9 +36,7 @@ def display(selected_lists: list, lists: dict) -> None:
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def print_headers(selected_lists: list, header_width: int) -> None:
-
-    visible_lists = selected_lists[1:]
+def print_headers(selected_list: str, visible_lists: list, header_width: int) -> None:
 
     # Centering individual headers
     centered_headers = [f'{header:^{header_width}}' for header in visible_lists]
@@ -42,12 +46,11 @@ def print_headers(selected_lists: list, header_width: int) -> None:
     RESET = '\033[0m'
 
     # Styling header of selected list
-    original = selected_lists[0]
-    styled = f'{UNDERLINED + original + RESET}'
-    pos = visible_lists.index(original)
+    styled = f'{UNDERLINED + selected_list + RESET}'
+    pos = visible_lists.index(selected_list)
 
     # Injecting the styled header
-    centered_headers[pos] = centered_headers[pos].replace(original, styled)
+    centered_headers[pos] = centered_headers[pos].replace(selected_list, styled)
 
     # Printing formatted headers
     formatted_headers = f"| {' | '.join(centered_headers)} |"
